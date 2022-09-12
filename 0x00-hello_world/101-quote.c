a7308ba246a2c0c3965ca03b72ee9fa37caa8ff7
OsbertKing <okereaforosbert@gmail.com>
2022-09-11 17:27:16 -0700

    Fixing bugs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,8 @@
  * main -Entry point
  * *
  * Return: Always 1 (Success)
- * */
-int main (void)
+ */
+int main(void)
 {
 	write(2,
 	"and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
