3ad2fccabbcc541fcc79c82656d8897ebef6d511
OsbertKing <okereaforosbert@gmail.com>
2022-10-14 22:13:23 -0700

    fixing bugs

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,12 +12,12 @@ int sum_them_all(const unsigned int n, ...)
 	register unsigned int i;
 	int sum = 0;
 
-	va_list arguements;
+	va_list arguments;
 
-	va_start(arguements, n);
+	va_start(arguments, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(arguements, int);
-	va_end(arguements);
+		sum += va_arg(arguments, int);
+	va_end(arguments);
 	return (sum);
 }
